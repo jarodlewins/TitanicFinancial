149f8f372c4fdc151dea2923f5da65184d713fad
jarodlewins <38117671+jarodlewins@users.noreply.github.com>
2019-06-06 13:42:57 -0700

    initiate files on this repo

---
+++ Titanic/js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_ZkVBl9pzq', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '52heph9k1m14ihmiv493f3uh5o' //is this used anywhere?
+    },
+};
